401640d319bc5750680df0d4426b4ef2e052ba42
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-12-20 20:38:25 +0200

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,7 @@
+import PropTypes from "prop-types";
+import styles from '../Profile/Profile.module.css';
+
+console.log(styles);
 export const Profile = ({username,tag,location,avatar,stats}) => {
     return (
         <div class="profile">
@@ -31,19 +35,19 @@ export const Profile = ({username,tag,location,avatar,stats}) => {
   };
 
 
-  // Profile.propTypes = {    // 
-  // username: PropTypes.string.isRequired,
-  // tag: PropTypes.string.isRequired,
-  // avatar: PropTypes.string.isRequired,
-  // location: PropTypes.string.isRequired,
-
-    //     friends: PropTypes.arrayOf(
-    //         PropTypes.shape({            
-    //             followers: PropTypes.string.isRequired,
-    //             amount_followers: PropTypes.string.isRequired
-    //             views: PropTypes.string.isRequired,
-    //             amount_views: PropTypes.string.isRequired, 
-    //             likes: PropTypes.string.isRequired,
-    //             amount_likes: PropTypes.string.isRequired
-    //         }))
-    // }
+Profile.propTypes = {    // 
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({            
+      followers: PropTypes.string.isRequired,
+      amount_followers: PropTypes.string.isRequired,
+      views: PropTypes.string.isRequired,
+      amount_views: PropTypes.string.isRequired, 
+      likes: PropTypes.string.isRequired,
+      amount_likes: PropTypes.string.isRequired
+    })
+  )
+}
