2315f44b7bee49d61a13bf69a1bba89e1edec9fa
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-12-20 20:38:23 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,25 +1,42 @@
+import PropTypes from "prop-types";
+import './FriendList';
+
+
 export const FriendList = ({friends}) => {
     return (
         <ul class="friend-list">
-            {friends.map((friend)=>{
+            {friends.map((friend)=>{                
                return (
                     <li class="item" key={friend}>
-                        <span class="status"></span>
+                        <div class="status"
+                            style={{
+                                height:'6px',
+                                width:'6px',
+                                borderRadius:'50%',           
+                                backgroundColor: randomColor(friend.isOnline)  
+                            }}
+                        ></div>
                         <img class="avatar" src={friend.avatar} alt="User avatar" width="48" />
                         <p class="name">{friend.name}</p>
                     </li> 
                 )              
-            })}
-            
+            })}            
         </ul>
     );
 };
 
+const randomColor = (item) => {
+    return item === true ? 'red': 'green'
+}
 
-// FriendList.propTypes = {    //     
-    //     friends: PropTypes.arrayOf(
-    //         PropTypes.shape({            
-    //             name: PropTypes.string.isRequired,
-    //             avatar: PropTypes.string.isRequired
-    //         }))
-    // }
+
+FriendList.propTypes = {       
+    friends: PropTypes.arrayOf(
+        PropTypes.shape({            
+            name: PropTypes.string.isRequired,
+            avatar: PropTypes.string.isRequired,
+            isOnline: PropTypes.bool
+        })
+    )
+}
+
