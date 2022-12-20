d19368fd4541525798e1d2ce3ce5f6c770022cf7
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-12-20 16:26:53 +0200

    Create Profile.jsx

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,31 @@
+export const Profile = ({username,tag,location,avatar,stats}) => {
+    return (
+        <div class="profile">
+        <div class="description">
+          <img
+            src={avatar}
+            alt="User avatar"
+            class="avatar"
+          />
+          <p class="name">{username}</p>
+          <p class="tag">{tag}</p>
+          <p class="location">{location}</p>
+        </div>
+      
+        <ul class="stats">
+          <li>
+            <span class="label">{stats.followers}</span>
+            <span class="quantity">{stats.amount_followers}</span>
+          </li>
+          <li>
+            <span class="label">{stats.views}</span>
+            <span class="quantity">{stats.amount_views}</span>
+          </li>
+          <li>
+            <span class="label">{stats.likes}</span>
+            <span class="quantity">{stats.amount_likes}</span>
+          </li>
+        </ul>
+      </div>
+    );
+  };
