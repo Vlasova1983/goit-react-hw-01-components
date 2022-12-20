5ee9150b4f893f3ebf026809a0d22ee4887801a8
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-12-20 18:01:32 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,25 +1,28 @@
 export const Statistics = ({title,stats}) => {
     return (
         <section class="statistics">
-            <h2 class="title">{title}</h2>
+            {title && <h2>{title}</h2>}
             <ul class="stat-list">
-                <li class="item">
-                    <span class="label">{stats.statistics.docx}</span>
-                    <span class="percentage">{stats.statistics.amount_docx}</span>
-                </li>
-                <li class="item">
-                    <span class="label">{stats.statistics.pdf}</span>
-                    <span class="percentage">{stats.statistics.amount_pdf}</span>
-                </li>
-                <li class="item">
-                    <span class="label">{stats.statistics.mp3}</span>
-                    <span class="percentage">{stats.statistics.amount_mp3}</span>
-                </li>
-                <li class="item">
-                    <span class="label">{stats.statistics.mp4}</span>
-                    <span class="percentage">{stats.statistics.amount_mp4}</span>
-                </li>
+                {stats.map((stat)=>{
+                    return (
+                        <li class="item" key={stat}>
+                            <span class="label">{stat.name}</span>
+                            <span class="percentage">{stat.amount}</span>
+                        </li>
+                    )              
+                })}                
             </ul>
         </section>
     );
 };
+
+// Statistics.propTypes = {
+//     title: PropTypes.string.isRequired,
+//     stats: PropTypes.arrayOf(
+//         PropTypes.shape({            
+//             name: PropTypes.string.isRequired,
+//             amount: PropTypes.string.isRequired
+//         }))
+// }
+
+
