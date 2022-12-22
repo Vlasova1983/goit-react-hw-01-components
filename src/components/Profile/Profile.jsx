f809a75a51a0fd6155663cd649c81a4509ea0dab
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-12-22 09:37:29 +0200

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,41 +1,39 @@
 import PropTypes from "prop-types";
 import styles from '../Profile/Profile.module.css';
 
-console.log(styles);
 export const Profile = ({username,tag,location,avatar,stats}) => {
-  console.log(avatar);
-    return (
-        <div className={styles.profile}>
-        <div className={styles.description}>
-          <img
-            src={avatar}
-            alt="User avatar"
-            className={styles.avatar}
-          />
-          <p className={styles.name}>{username}</p>
-          <p>{tag}</p>
-          <p>{location}</p>
-        </div>      
-        <ul className={styles.stats}>
-          <li className={styles.stat}>
-            <span>{stats.followers}</span>
-            <span className={styles.quantity}>{stats.amount_followers}</span>
-          </li>
-          <li className={styles.stat}>
-            <span>{stats.views}</span>
-            <span className={styles.quantity}>{stats.amount_views}</span>
-          </li>
-          <li className={styles.stat}>
-            <span>{stats.likes}</span>
-            <span className={styles.quantity}>{stats.amount_likes}</span>
-          </li>
-        </ul>
-      </div>
-    );
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img
+          src={avatar}
+          alt="User avatar"
+          className={styles.avatar}
+        />
+        <p className={styles.name}>{username}</p>
+        <p>{tag}</p>
+        <p>{location}</p>
+      </div>      
+      <ul className={styles.stats}>
+        <li className={styles.stat}>
+          <span>{stats.followers}</span>
+          <span className={styles.quantity}>{stats.amount_followers}</span>
+        </li>
+        <li className={styles.stat}>
+          <span>{stats.views}</span>
+          <span className={styles.quantity}>{stats.amount_views}</span>
+        </li>
+        <li className={styles.stat}>
+          <span>{stats.likes}</span>
+          <span className={styles.quantity}>{stats.amount_likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
   };
 
 
-Profile.propTypes = {    // 
+Profile.propTypes = {    
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
