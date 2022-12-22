0d74472edfc5359754eed5df7d718027441165cc
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-12-22 09:37:23 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,42 +1,18 @@
 import PropTypes from "prop-types";
 import styles from '../FriendList/FriendList.module.css';
-
+import {Friend} from '../Friend/Friend';
 
 export const FriendList = ({friends}) => {
     return (
         <ul className={styles.friend_list}>
-            {friends.map((friend)=>{                
-               return (
-                    <li className={styles.item} key={friend}>
-                        <div 
-                            style={{
-                                height:'10px',
-                                width:'10px',
-                                borderRadius:'50%',           
-                                backgroundColor: randomColor(friend.isOnline)  
-                            }}
-                        ></div>
-                        <img className={styles.avatar} src={friend.avatar} alt="User avatar" width="48" />
-                        <p>{friend.name}</p>
-                    </li> 
-                )              
-            })}            
+            {friends.map((friend) => {
+             return <Friend friend={friend}/>
+            })}               
         </ul>
     );
 };
 
-const randomColor = (item) => {
-    return item === true ? 'red': 'green'
-}
-
-
 FriendList.propTypes = {       
-    friends: PropTypes.arrayOf(
-        PropTypes.shape({            
-            name: PropTypes.string.isRequired,
-            avatar: PropTypes.string.isRequired,
-            isOnline: PropTypes.bool
-        })
-    )
+    friends: PropTypes.arrayOf()
 }
 
